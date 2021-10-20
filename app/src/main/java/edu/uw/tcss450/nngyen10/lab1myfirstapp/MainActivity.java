b2f32fe4b15f969a35ed7c56324551a3ba10f995
Justinn253 <justinaschenbrenner@gmail.com>
2021-10-19 21:16:53 -0700

    Made a comment on line 15 in MainActivity.java

--- app/src/main/java/edu/uw/tcss450/nngyen10/lab1myfirstapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/nngyen10/lab1myfirstapp/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("onCreate", "Activities in onCreate() method");
+        // On Create comment gaming
     }
 
     @Override
