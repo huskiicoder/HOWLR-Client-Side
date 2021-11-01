f51f2e6940a3bee13ff3e35917b8ac6844904ef9
Justinn253 <justinaschenbrenner@gmail.com>
2021-10-31 19:05:47 -0700

    Added bottom navigation and implemented a simple fragment for home and friends that can be navigated to after logging in using the bottom navigation.

---
+++ app/src/main/java/edu/uw/tcss450/authentication/ui/friends/FriendsFragment.java
@@ -0,0 +1,24 @@
+package edu.uw.tcss450.authentication.ui.friends;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.authentication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FriendsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
